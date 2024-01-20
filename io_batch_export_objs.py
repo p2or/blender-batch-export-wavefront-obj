83fbf616102c725cfd614a89b9c6f442093b0f23
p2or <512368+p2or@users.noreply.github.com>
2024-01-20 23:45:43 +0100

    Blender 3.6+ updates

--- io_batch_export_objs.py
+++ io_batch_export_objs.py
@@ -20,8 +20,8 @@
 bl_info = {
     "name": "OBJ Batch Export",
     "author": "p2or, brockmann, trippeljojo",
-    "version": (0, 3, 0),
-    "blender": (3, 3, 0),
+    "version": (0, 4, 0),
+    "blender": (3, 6, 0),
     "location": "File > Import-Export",
     "description": "Export multiple OBJ files, their UVs and Materials",
     "warning": "",
@@ -55,9 +55,19 @@ class WM_OT_batchExportObjs(bpy.types.Operator, ExportHelper):
     filter_glob = StringProperty(
             default="*.obj;*.mtl",
             options={'HIDDEN'},)
-    
+
+    selection_only: BoolProperty(
+            name="Selection Only",
+            description="Export selected objects only",
+            default=True,)
+
+    scale_factor: FloatProperty(
+            name="Scale",
+            min=0.01, max=1000.0,
+            default=1.0,)
+            
     axis_forward: EnumProperty(
-            name="Axis Forward",
+            name="Forward Axis",
             items=(('X', "X", "Positive X Axis"),
                    ('Y', "Y", "Positive Y Axis"),
                    ('Z', "Z", "Positive Z Axis"),
@@ -67,7 +77,7 @@ class WM_OT_batchExportObjs(bpy.types.Operator, ExportHelper):
             default='NEGATIVE_Z')
             
     axis_up: EnumProperty(
-            name="Axis Up",
+            name="Up Axis",
             items=(('X', "X Up", "Positive X Axis"),
                    ('Y', "Y Up (Default)", "Positive Y Axis"),
                    ('Z', "Z Up", "Positive Z Axis"),
@@ -76,80 +86,111 @@ class WM_OT_batchExportObjs(bpy.types.Operator, ExportHelper):
                    ('NEGATIVE_Z', "-Z Up", "Negative Z Axis"),),
             default='Y')
 
-    scale_factor: FloatProperty(
-            name="Scale",
-            min=0.01, max=1000.0,
-            default=1.0,)
-    
-    selection_only: BoolProperty(
-            name="Selection Only",
-            description="Export selected objects only",
-            default=True,)
-            
-    eval_mode: EnumProperty(
-            name="Evaluation Mode",
-            items=(('DAG_EVAL_VIEWPORT', "Viewport (Default)", "Objects as they appear in the Viewport"),
-                   ('DAG_EVAL_RENDER', "Render", "Objects as they appear in Render"),),
-            default='DAG_EVAL_VIEWPORT')
-    
     modifiers_apply: BoolProperty(
             name="Apply Modifiers",
-            description="Apply Modifiers to exported meshes",
+            description="Apply modifiers to exported meshes",
             default=False)
             
+    eval_mode: EnumProperty(
+            name="Properties",
+            description="Determines properties like object visibility, "
+                        "modifiers etc., where they differ for Render and Viewport",
+            items=(('DAG_EVAL_VIEWPORT', 
+                    "Viewport (Default)", 
+                    "Export objects as they appear in the viewport"),
+                   ('DAG_EVAL_RENDER', 
+                    "Render", 
+                    "Export objects as they appear in render"),),
+            default='DAG_EVAL_VIEWPORT')
+            
+    # GEOMETRY 
     write_uvs: BoolProperty(
-            name="Include UVs",
-            description="Write out the active UV coordinates",
-            default=True)
-            
-    write_normals: BoolProperty(
-            name="Write Normals",
-            description="Export one normal per vertex and per face, to represent flat faces and sharp edges",
+            name="UV Coordinates",
+            description="Export the UV coordinates",
             default=True)
 
-    write_materials: BoolProperty(
-            name="Write Materials",
-            description="Write out the MTL file",
+    write_normals: BoolProperty(
+            name="Normals",
+            description="Export per-face normals if the face is flat-shaded, "
+                        "per-face-per-loop normals if smooth-shaded",
             default=True)
-            
-    triangulate_faces: BoolProperty(
-            name="Triangulate Faces",
-            description="Convert all faces to triangles",
+    
+    write_colors: BoolProperty(
+            name="Colors",
+            description="Export per-vertex colors",
             default=False)
 
-    write_nurbs: BoolProperty(
-            name="Write Nurbs",
-            description="Write nurbs curves as OBJ nurbs rather than "
-                        "converting to geometry",
+    triangulate_faces: BoolProperty(
+            name="Triangulated Mesh",
+            description='All ngons with four or more vertices will be triangulated. '
+                        'Meshes in the scene will not be affected. Behaves like '
+                        'Triangulate Modifier with ngon-method: "Beauty", quad-method: '
+                        '"Shortest Diagonal", min vertices: 4.',
             default=False)
     
+    write_nurbs: BoolProperty(
+            name="Curves as NURBS",
+            description="Export curves in parametric form instead of exporting as mesh",
+            default=False)
+        
+    # MATERIALS 
+    write_materials: BoolProperty(
+            name="Export Materials",
+            description="Export MTL library. There must be a Principled-BSDF "
+                        "node for image textures to be exported to the MTL file.",
+            default=True)
+    
+    write_pbr: BoolProperty(
+            name="PBR Extensions",
+            description="Export MTL library using PBR extensions (roughness, metallic, "
+                        "sheen, coat, anisotropy, transmission)",
+            default=False) 
+    
+    path_mode: EnumProperty(
+            name="Path Mode",
+            description="Method used to reference paths",
+            items=(('AUTO', "Auto", "Use relative paths with subdirectories only"),
+                   ('ABSOLUTE', "Absolute", "Always write absolute paths"),
+                   ('RELATIVE', "Relative", "Write relative paths where possible"),
+                   ('MATCH', "Match", "OMatch absolute/relative setting with input path"),
+                   ('STRIP', "Strip", "Write filename only"),
+                   ('COPY', "Copy", "Copy the file to the destination path"),
+                   ),
+            default='AUTO')
+    
+    # GROUPING 
     group_by_object: BoolProperty(
-            name="Objects as OBJ Groups ",
-            description="",
+            name="Object Groups",
+            description="Append mesh name to object name, separated by a '_'",
             default=False)
     
     group_by_material: BoolProperty(
             name="Material Groups",
-            description="",
+            description="Generate an OBK group for each part of a geometry "
+                        "using a different material",
             default=False)
     
     group_by_vertex: BoolProperty(
-            name="Polygroups",
-            description="",
+            name="Vertex Groups",
+            description="Export the name of the vertex group of a face. "
+                        "It is approximated by choosing the vertex group "
+                        "with the most members amoung the vertices of a face",
             default=False)
-            
+    
     smoothing_groups: BoolProperty(
             name="Smooth Groups",
-            description="Write sharp edges as smooth groups",
+            description='Every smooth-shaded face is assigned group "1" and '
+                        'every flat-shaded face "off".',
             default=False)
 
     smoothing_group_bitflags: BoolProperty(
-            name="Bitflag Smooth Groups",
+            name="Smooth Group Bitflags",
             description="Same as 'Smooth Groups', but generate smooth groups IDs as bitflags "
                         "(produces at most 32 different smooth groups, usually much less)",
             default=False)
 
-    def execute(self, context):                
+    def execute(self, context):
+                       
         # Get the current folder
         folder_path = os.path.dirname(self.filepath)
         
@@ -168,20 +209,25 @@ class WM_OT_batchExportObjs(bpy.types.Operator, ExportHelper):
             bpy.ops.wm.obj_export(
                     filepath=file_path,
                     export_animation=False,
+                    #start_frame=-2147483648, 
+                    # end_frame=2147483647,
                     forward_axis=self.axis_forward,
                     up_axis=self.axis_up,
-                    scaling_factor=self.scale_factor,
+                    global_scale=self.scale_factor,
                     apply_modifiers=self.modifiers_apply,
-                    export_selected_objects=True,
                     export_eval_mode=self.eval_mode,
+                    export_selected_objects=True,
                     export_uv=self.write_uvs,
                     export_normals=self.write_normals,
+                    export_colors=self.write_colors,
                     export_materials=self.write_materials,
+                    export_pbr_extensions=self.write_pbr,
+                    path_mode=self.path_mode,
                     export_triangulated_mesh=self.triangulate_faces,
                     export_curves_as_nurbs=self.write_nurbs,
                     export_object_groups=self.group_by_object,
-                    export_material_groups = self.group_by_material,
-                    export_vertex_groups = self.group_by_vertex,
+                    export_material_groups=self.group_by_material,
+                    export_vertex_groups=self.group_by_vertex,
                     export_smooth_groups=self.smoothing_groups,
                     smooth_group_bitflags=self.smoothing_group_bitflags
             )
@@ -199,15 +245,15 @@ def menu_func_import(self, context):
 
 def register():
     bpy.utils.register_class(WM_OT_batchExportObjs)
-    bpy.types.TOPBAR_MT_file_export.append(menu_func_import)
+    #bpy.types.TOPBAR_MT_file_export.append(menu_func_import)
 
 def unregister():
     bpy.utils.unregister_class(WM_OT_batchExportObjs)
-    bpy.types.TOPBAR_MT_file_export.remove(menu_func_import)
+    #bpy.types.TOPBAR_MT_file_export.remove(menu_func_import)
 
 
 if __name__ == "__main__":
     register()
 
     # test call
-    #bpy.ops.export_scene.batch_obj('INVOKE_DEFAULT')
+    bpy.ops.export_scene.batch_obj('INVOKE_DEFAULT')
